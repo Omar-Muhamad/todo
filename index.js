fc25e3e904dcf454480da9ccbc1d6cc8420f9dcf
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2024-03-28 11:50:33 +0200

    Add strategy

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello')
+const todo = {
+  index: 1,
+  description: '',
+  isCompleted: false
+}
+
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+const filteredTodos = () => {
+
+}
+
+// addTodo function
+
+// toggleTodoStatus function
+
+// deleteTodo function 
+
+//  clearAllCompleted
