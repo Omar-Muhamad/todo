41bd46fb4bb1959e3d8ac7696682705341487157
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2024-03-28 12:33:10 +0200

    Add renderTodos function

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const todo = {
   index: 1,
-  description: '',
+  description: 'asdfghj',
   isCompleted: false
 }
 
-const todos = JSON.parse(localStorage.getItem('todos'))
+// const todos = JSON.parse(localStorage.getItem('todos'))
 
-const filteredTodos = () => {
+// const filteredTodos = () => {
 
-}
+// }
+
+localStorage.setItem('todos', JSON.stringify([todo]))
+
+const renderTodos = () => {
+  const todos = JSON.parse(localStorage.getItem('todos'));
+  const todoList = document.getElementsByClassName('todo-list')[0]
+  todoList.innerHTML = '';
+  if (!todos.length) return;
+  todos.sort((a, b) => (a.index - b.index)).forEach((todo) => {
+    const checked = todo.completed;
+    const item = `
+      <li class="item" data-index=${todo.index}>
+        <input  ${checked === true ? 'checked' : ''} type="checkbox" class="checkBox" data-index=${todo.index}>
+        <p type="text" class="toDoText">${todo.description}</p>
+        <button class="deleteBtn" data-index=${todo.index}><i class="far fa-trash-alt" ></i></button>
+      </li>
+    `;
+    todoList.innerHTML += item;
+  });
+};
+
+renderTodos()
 
 // addTodo function
 
