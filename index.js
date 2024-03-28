bb15c016b29faea2f50ee49cbd44ef394c14de4e
Solovic2 <islammohamed323@gmail.com>
2024-03-28 12:57:45 +0200

    fill toggleTodoStatus and clearAllCompleted functions

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const todo = {
   index: 1,
-  description: 'asdfghj',
-  isCompleted: false
-}
+  description: "asdfghj",
+  isCompleted: false,
+};
 
 // const todos = JSON.parse(localStorage.getItem('todos'))
 
@@ -10,32 +10,57 @@ const todo = {
 
 // }
 
-localStorage.setItem('todos', JSON.stringify([todo]))
+localStorage.setItem("todos", JSON.stringify([todo]));
 
 const renderTodos = () => {
-  const todos = JSON.parse(localStorage.getItem('todos'));
-  const todoList = document.getElementsByClassName('todo-list')[0]
-  todoList.innerHTML = '';
+  const todos = JSON.parse(localStorage.getItem("todos"));
+  console.log(todos);
+  const todoList = document.getElementsByClassName("todo-list")[0];
+  todoList.innerHTML = "";
   if (!todos.length) return;
-  todos.sort((a, b) => (a.index - b.index)).forEach((todo) => {
-    const checked = todo.completed;
-    const item = `
+  todos
+    .sort((a, b) => a.index - b.index)
+    .forEach((todo) => {
+      const checked = todo.isCompleted;
+      const item = `
       <li class="item" data-index=${todo.index}>
-        <input  ${checked === true ? 'checked' : ''} type="checkbox" class="checkBox" data-index=${todo.index}>
+        <input  ${
+          checked === true ? "checked" : ""
+        } type="checkbox" class="checkBox" data-index=${todo.index}>
         <p type="text" class="toDoText">${todo.description}</p>
-        <button class="deleteBtn" data-index=${todo.index}><i class="far fa-trash-alt" ></i></button>
+        <button class="deleteBtn" data-index=${
+          todo.index
+        }><i class="far fa-trash-alt" ></i></button>
       </li>
     `;
-    todoList.innerHTML += item;
-  });
+      todoList.innerHTML += item;
+    });
 };
 
-renderTodos()
+renderTodos();
 
 // addTodo function
 
 // toggleTodoStatus function
+const toggleTodoStatus = (todoElement) => {
+  const todos = JSON.parse(localStorage.getItem("todos"));
+  todos.forEach((element) => {
+    if (element.index === todoElement.index) {
+      element.isCompleted = !element.isCompleted;
+    }
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
+  renderTodos();
+};
 
-// deleteTodo function 
+// deleteTodo function
 
 //  clearAllCompleted
+const clearAllCompleted = () => {
+  const todos = JSON.parse(localStorage.getItem("todos"));
+  todos.forEach((element) => {
+    element.isCompleted = false;
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
+  renderTodos();
+};
